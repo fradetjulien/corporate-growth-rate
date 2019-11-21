88eb1fdd4d83e1df81c200433600a449150a2bee
Julien <julien.fradet5@gmail.com>
2019-11-21 00:37:42 -0500

    All values are now retrieved

--- module/index.py
+++ module/index.py
@@ -1,11 +1,71 @@
 import click
 from selenium import webdriver
 
+def scrap_values(data, driver, path, variable):
+    '''
+    Scrap the Total Revenue of the Company from the data table loaded previously
+    '''
+    index = 1
+    try:
+        while index < 7:
+            data[variable].append(driver.find_element_by_xpath(path.format(index)).text)
+            index = index + 1
+        data[variable].pop(0)
+    except:
+        print('Sorry, failure while retrieving {}.'.format(variable))
+    return data
+
+def scrap_company_name(data, driver):
+    '''
+    Scrap the Company name from the data table loaded previously
+    '''
+    try:
+        data["company_name"] = driver.find_element_by_xpath(\
+                               '//*[@id="quote-header-info"]/div[2]/div[1]/div[1]/h1').text
+    except:
+        print('Sorry, failure while retrieving Company name.')
+    return data
+
+def init_data():
+    '''
+    Initialize a new dictionnary and set empty variables inside
+    '''
+    data = {
+        "company_name": None,
+        "total_revenue": [],
+        "net_income": [],
+        "dates": []
+    }
+    return data
+
 def scrap_data(driver):
     '''
-    Scrap the total revenue and the net income
+    Scrap all needed values for future computes and store them into a dictionnary
     '''
-    return
+    data = init_data()
+    data = scrap_company_name(data, driver)
+    data = scrap_values(data, driver,\
+                       '//*[@id="Col1-1-Financials-Proxy"]/section/div[4]/div[1]/\
+                       div[1]/div[2]/div[1]/div[1]/div[{}]', 'total_revenue')
+    data = scrap_values(data, driver,\
+                       '//*[@id="Col1-1-Financials-Proxy"]/section/div[4]/div[1]/\
+                       div[1]/div[2]/div[11]/div[1]/div[{}]', 'net_income')
+    data = scrap_values(data, driver,\
+                        '//*[@id="Col1-1-Financials-Proxy"]/section/div[4]/div[1]/\
+                        div[1]/div[1]/div/div[{}]', 'dates')
+    return data
+
+def is_result_available(driver):
+    '''
+    Check if the loaded page has available data
+    '''
+    try:
+        path = '//*[@id="quote-summary"]/div[1]/table/tbody/tr[1]/td[2]/span'
+        error = driver.find_element_by_xpath(path)
+        if error and 'N/A' in error.text:
+            return True
+    except:
+        return False
 
 def is_result_empty(driver):
     '''
@@ -26,7 +86,7 @@ def load_data(ticker):
     try:
         driver = webdriver.Chrome(executable_path=".//chromedriver")
         driver.get("https://finance.yahoo.com/quote/" + ticker + "/financials?p=" + ticker)
-        if is_result_empty(driver):
+        if is_result_empty(driver) or is_result_available(driver):
             driver.quit()
             return None
     except:
